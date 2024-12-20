434c51f1a8567750265bf14cdd9e6d1082c3d350
actuallylemoncurd <curtis.x.jenkins@gmail.com>
2024-12-20 14:32:59 -0500

    eEPB; add EPB msg to send

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -26,26 +26,36 @@ int ACS_Sta_ADR;
 double vEgo;
 
 typedef struct {
-    bool EPB_enable;
-    double EPB_brake;
-    bool EPB_active;
+  bool EPB_enable;
+  double EPB_brake;
+  bool EPB_active;
 } EPB_Handler_Result;
 
 typedef struct {
-    bool EPB_enable;
-    bool EPB_enable_prev;
-    bool EPB_enable_2old;
-    bool ACC_anz_blind;
-    int EPB_counter;
-    int ACC_anz_blind_counter;
-    double EPB_brake;
-    double EPB_brake_last;
+  bool EPB_enable;
+  bool EPB_enable_prev;
+  bool EPB_enable_2old;
+  bool ACC_anz_blind;
+  int EPB_counter;
+  int ACC_anz_blind_counter;
+  double EPB_brake;
+  double EPB_brake_last;
 } EPB_State;
 
 typedef struct {
-    bool gasPressed;
-    bool brakePressed;
+  bool gasPressed;
+  bool brakePressed;
 } CarState;
+
+typedef struct {            //  Definition           offset  scale      range      sB sb  len
+  uint8_t EP_COUNTER;       //  4 position counter      0      1        0..15       1:[0 | 4]
+  uint8_t EP_brakeReq;      //  ECD brake request    -7.968    1   -7.968..4.224    4:[0 | 8]
+  uint8_t EP_brakeEnable;   //  Enable brakeReq         0      1        0..1        5:[1 | 1]
+  uint8_t EP_epbAvailable;  //  EPB is available        0      1        0..1        5:[3 | 1]
+  uint8_t EP_brakeLights;   //  Enable brake lights     0      1        0..1        5:[6 | 1]
+  uint8_t EP_HydHalten;     //  ECD standstill          0      1        0..1        7:[0 | 1]
+  uint8_t EP_CHECKSUM;      //  XOR checksum            0      1        0..255      8:[0 | 8]
+} EPB_msg;
 
 double limit_jerk(double accel, double prev_accel, double max_jerk, double dt) {
     double max_delta_accel = max_jerk * dt;
